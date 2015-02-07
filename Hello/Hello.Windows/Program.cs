f5d17c9e47051f70db99f96d610ff3d918a349fb
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-02-06 17:22:32 -0800

    Followed tutorial, have running app
    Using tutorial at
    http://blog.craftworkgames.com/getting-started-with-astrid

---
+++ Hello/Hello.Windows/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Astrid.Windows;
+
+namespace Hello.Windows
+{
+    class Program
+    {
+        static void Main()
+        {
+            var config = new WindowsApplicationConfig
+            {
+                Title = "Hello Astrid",
+                Width = 800,
+                Height = 480,
+                ContentPath = "Content"
+            };
+
+            using (var application = new WindowsApplication(config))
+            {
+                var game = new Game(application);
+                application.Run(game);
+            }
+        }
+    }
+}
---
+++ Hello/Hello/Game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Astrid.Framework;
+using Astrid.Core;
+using Astrid.Framework.Graphics;
+using Astrid.Framework.Assets;
+namespace Hello
+{
+    public class Game : GameBase
+    {
+        private SpriteBatch spriteBatch;
+        private Texture texture;
+        private Vector2 position;
+
+        public Game(ApplicationBase application)
+            : base(application)
+        {
+        }
+
+        public override void Create()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            texture = AssetManager.Load<Texture>("Man.png");
+
+            var x = GraphicsDevice.Width / 2 - texture.Width / 2;
+            var y = GraphicsDevice.Height / 2 - texture.Height / 2;
+            position = new Vector2(x, y);
+        }
+
+        public override void Destroy()
+        {
+        }
+
+        public override void Pause()
+        {
+        }
+
+        public override void Resume()
+        {
+        }
+
+        public override void Resize(int width, int height)
+        {
+        }
+
+        public override void Update(float deltaTime)
+        {
+        }
+
+        public override void Render(float deltaTime)
+        {
+            GraphicsDevice.Clear(Color.SkyBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, position);
+            spriteBatch.End();
+        }
+    }
+}
